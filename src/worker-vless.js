f89eb83001464be8e62f61a63d87a18ee6fe1b02
adsf240 <75925577+adsf240@users.noreply.github.com>
2026-07-02 11:47:34 +0800

    Update worker-vless.jsFix: revert broken edits, restore let userID

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -1,10 +1,8 @@
 // <!--GAMFC-->version base on commit 58686d5d125194d34a1137913b3a64ddcf55872f, time is 2024-11-27 09:26:01 UTC<!--GAMFC-END-->.
 // @ts-ignore
 import { connect } from 'cloudflare:sockets';
-unction isValidUUID(uuid) {
-	    return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(uuid);
-}
-let proxyIP = '';
+let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+
 
 
 if (!isValidUUID(userID)) {
